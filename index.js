d741ed5fa3cda87b1cc8be6ab99378d77537c33f
Daniel Parra <danialepaco@Daniels-MacBook-Pro.local>
2018-02-26 12:01:20 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import a library to create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
+
+// Render it to device
+AppRegistry.registerComponent('albums', () => App);
