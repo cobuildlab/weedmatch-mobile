1fab04286744fe2d8edf9627fdafe3bf56699a24
daniel parra <danialepaco@hotmail.com>
2018-08-17 17:25:25 -0400

    change url

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -45,7 +45,7 @@ _handleWebSocketSetup() {
 
   const { navigation } = this.props;
   const chat_id = navigation.getParam('chat_id', '0');
-  this.socket = new WebSocket("ws://192.168.0.16:8888/ws?"+'id_user='+APP_STORE.getId()+'&'+'username='+APP_STORE.getUser()+'&'+'chat_id='+chat_id+'&'+'token='+APP_STORE.getToken());
+  this.socket = new WebSocket("ws://dev-api.weedmatch.cl:8888/ws?"+'id_user='+APP_STORE.getId()+'&'+'username='+APP_STORE.getUser()+'&'+'chat_id='+chat_id+'&'+'token='+APP_STORE.getToken());
 
   this.socket.onopen = () => {
     this.setState({connected:true})
