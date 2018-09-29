ea7d7af0f457746c46401cb1c29709b55a2e2da2
Angel Lacret <alacret@gmail.com>
2018-09-29 12:52:58 -0400

    finally working with Android

--- android/app/src/main/java/co/fourgeeks/weedmatch/MainApplication.java
+++ android/app/src/main/java/co/fourgeeks/weedmatch/MainApplication.java
@@ -2,28 +2,29 @@ package co.fourgeeks.weedmatch;
 
 import android.app.Application;
 
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactApplication;
+
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+
 import com.toast.RCTToastPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.beefe.picker.PickerViewPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-
-import io.invertase.firebase.RNFirebasePackage;
-
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
+
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // <-- Add this line
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
-
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,6 +37,13 @@ public class MainApplication extends Application implements ReactApplication {
         return mCallbackManager;
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        AppEventsLogger.activateApp(this);
+    }
+
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
@@ -46,16 +54,16 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new RNFirebasePackage(),
+                    new RNFirebaseNotificationsPackage(),
+                    new RNFirebaseMessagingPackage(),
                     new RCTToastPackage(),
                     new RNSpinkitPackage(),
                     new PickerViewPackage(),
                     new PickerPackage(),
                     new RNI18nPackage(),
-                    new RNFirebasePackage(),
-                    new FBSDKPackage(mCallbackManager),
                     new RNDeviceInfo(),
-                    new RNFirebaseNotificationsPackage(),
-                    new RNFirebaseMessagingPackage(),
+                    new FBSDKPackage(mCallbackManager),
                     new RNFirebaseAnalyticsPackage() // <-- Add this line
             );
         }
@@ -69,12 +77,5 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
-        AppEventsLogger.activateApp(this);
     }
 }
