fa034d0b1af0902abd26ba98ce74115ba8a62245
danlugo92 <danlugo92@gmail.com>
2018-10-23 17:49:21 -0400

    add eslint, prettier, eslint-prettier with sensible rules for react-native

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
+const reactVersion = require("./package.json").dependencies.react;
+
 module.exports = {
-    "extends": "google",
-    "rules": {
-        "object-curly-spacing": 0
-    },
-    "parserOptions": {
-        "ecmaVersion": 7,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true,
-        }
-    },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-native/all",
+    "eslint-config-prettier"
+  ],
+  plugins: ["react", "react-native"],
+  env: {
+    "react-native/react-native": true
+  },
+  rules: {},
+  settings: {
+    react: {
+      version: reactVersion
+    }
+  },
+  parser: "babel-eslint"
 };
