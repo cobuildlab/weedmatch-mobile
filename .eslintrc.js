75dd6f6aae7823075e68aa61b365ee4f5ff559c9
danlugo92 <danlugo92@gmail.com>
2018-10-24 15:12:26 -0400

    prettify .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
-const reactVersion = require("./package.json").dependencies.react;
+const reactVersion = require('./package.json').dependencies.react;
 
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-native/all",
-    "eslint-config-prettier"
-  ],
-  plugins: ["react", "react-native"],
-  env: {
-    "react-native/react-native": true
-  },
-  rules: {},
-  settings: {
-    react: {
-      version: reactVersion
-    }
-  },
-  parser: "babel-eslint"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-native/all',
+        'eslint-config-prettier',
+    ],
+    plugins: ['react', 'react-native'],
+    env: {
+        'react-native/react-native': true,
+    },
+    rules: {},
+    settings: {
+        react: {
+            version: reactVersion,
+        },
+    },
+    parser: 'babel-eslint',
 };
