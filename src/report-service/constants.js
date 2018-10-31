3c35f8a31a518330b4d6a340b095a2016d3527fa
danlugo92 <danlugo92@gmail.com>
2018-10-31 11:22:29 -0400

    complete typings, validations and constants for the report service

--- src/report-service/constants.js
+++ src/report-service/constants.js
@@ -1,10 +1,12 @@
 import { URL as GLOBAL_URL } from '../utils';
 /**
- * @typedef {import('./definitions').ReasonEnum} ReasonEnum
+ * @typedef {import('./typings').PlaceEnum} PlaceEnum
+ * @typedef {import('./typings').ReasonEnum} ReasonEnum
  */
 
 /**
  * Valid reasons for making a report
+ * @type {{ [K: string]: ReasonEnum }}
  */
 export const reportReasonEnum = {
     BULLYING: 'Harassment or bullying',
@@ -15,6 +17,17 @@ export const reportReasonEnum = {
     SALE: 'Sale or promotion of drugs',
     SPAM: 'Spam or Fraud',
     VIOLENCE: 'Violence or threat of violence',
+};
+
+/**
+ * Valid values for the key `place` when for making a report, depending on the
+ * type of the report being made
+ * @type {{ [k in PlaceEnum]: PlaceEnum }}
+ */
+export const placeEnum = {
+    Chat: 'Chat',
+    Feed: 'Feed',
+    Profile: 'Profile',
 };
 
 export const REPORT_API_ENDPOINT_URL = GLOBAL_URL + 'reported/';
