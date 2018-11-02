5f135d78ee4d8930b8cacffa2ffc93ac3a7bd2cb
danlugo92 <danlugo92@gmail.com>
2018-11-02 15:17:40 -0400

    typings

--- src/screens/chat/ChatTitle.js
+++ src/screens/chat/ChatTitle.js
@@ -1,27 +1,37 @@
-import React from "react";
-import {Text, View} from "react-native";
-import styles from "../../components/Message/style";
-import FastImage from "react-native-fast-image";
+import React from 'react';
+
+import * as PropTypes from 'prop-types';
+import { Text, View } from 'react-native';
+import FastImage from 'react-native-fast-image';
+
+import styles from '../../components/Message/style';
+
+/**
+ * @typedef {object} ChatTitleProps
+ * @prop {string} name Username
+ * @prop {string} src Image source
+ */
 
 /**
  * Component to show the header of the Chat Window
- * @param props
- * @return {*}
- * @constructor
+ * @type {React.SFC<ChatTitleProps>}
  */
-const ChatTitle = (props) => {
-    const {src, name} = props;
-    return (
-        <View style={styles.viewMsg}>
-            <FastImage style={styles.imgProfileItem}
-                       resizeMode={FastImage.resizeMode.contain}
-                       source={{uri: src}}
-            />
-            <View style={styles.viewTexts}>
-                <Text style={styles.textUser}>{name}</Text>
-            </View>
+const ChatTitle = ({ name, src }) => (
+    <View style={styles.viewMsg}>
+        <FastImage
+            style={styles.imgProfileItem}
+            resizeMode={FastImage.resizeMode.contain}
+            source={{ uri: src }}
+        />
+        <View style={styles.viewTexts}>
+            <Text style={styles.textUser}>{name}</Text>
         </View>
-    );
-}
+    </View>
+);
+
+ChatTitle.propTypes = {
+    name: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+};
 
 export default ChatTitle;
