1efc6dbc2d4a4e438ba012eaee423f500621f0ba
danlugo92 <danlugo92@gmail.com>
2018-11-06 15:19:19 -0400

    typings, linting, formatting

--- src/modules/chat/ChatTitle.js
+++ src/modules/chat/ChatTitle.js
@@ -1,30 +1,37 @@
+/**
+ * @prettier
+ */
 import React from 'react';
 import * as PropTypes from 'prop-types';
-import {Text, View, TouchableHighlight} from "react-native";
-import {Image} from 'react-native';
+import { Text, View, TouchableHighlight } from 'react-native';
+import { Image } from 'react-native';
 import styles from '../../components/Message/style';
 
 /**
  * @typedef {object} ChatTitleProps
  * @prop {string} name Username
  * @prop {string} src Image source
+ * @prop {() => void} onPress
  */
 
 /**
  * Component to show the header of the Chat Window
  * @type {React.SFC<ChatTitleProps>}
  */
-const ChatTitle = ({src, name, onPress}) => {
+const ChatTitle = ({ name, onPress, src }) => {
     return (
         <View style={styles.viewMsg}>
             <TouchableHighlight onPress={onPress}>
-                <Image style={styles.imgProfileItem}
-                           source={{uri: src}}
-                           onPress={onPress}
+                <Image
+                    style={styles.imgProfileItem}
+                    source={{ uri: src }}
+                    onPress={onPress}
                 />
             </TouchableHighlight>
             <View style={styles.viewTexts}>
-                <Text style={styles.textUser} onPress={onPress}>{name}</Text>
+                <Text style={styles.textUser} onPress={onPress}>
+                    {name}
+                </Text>
             </View>
         </View>
     );
@@ -32,8 +39,8 @@ const ChatTitle = ({src, name, onPress}) => {
 
 ChatTitle.propTypes = {
     name: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired,
     src: PropTypes.string.isRequired,
-    onPress: PropTypes.func.isRequired
 };
 
 export default ChatTitle;
