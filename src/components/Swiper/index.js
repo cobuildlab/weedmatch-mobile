76c41a084fe593642e3538e56e787bf617a09969
Abraham Jose Gonzalez Barboza <jgab.net@gmail.com>
2018-11-11 20:26:15 -0300

    fix: double import

--- src/components/Swiper/index.js
+++ src/components/Swiper/index.js
@@ -24,10 +24,7 @@ import textStyles from "../../styles/text";
 import GeoStore from "../../utils/GeoStore";
 import { PLACE_ENUM } from '../../modules/report';
 
-import REPORT_ROUTE_KEY from '../../modules/report'
-import { PLACE_ENUM } from '../../modules/report';
-
-import REPORT_ROUTE_KEY from '../../modules/report'
+import REPORT_ROUTE_KEY from '../../modules/report';
 
 export default class SwiperView extends Component {
     constructor(props) {
