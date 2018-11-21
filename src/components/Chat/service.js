b634ceadefdc28f07a603cd2120b34fff769a32b
danlugo92 <danlugo92@gmail.com>
2018-11-21 11:47:08 -0400

    logging

--- src/components/Chat/service.js
+++ src/components/Chat/service.js
@@ -1,7 +1,15 @@
 import { authHeader } from '../../utils';
 import { APP_STORE } from '../../Store';
 
+/**
+ * @param {string} pagUrl
+ */
 function chats(pagUrl) {
+    iif (__DEV__) {
+        // eslint-disable-next-line no-console
+        console.log('Chat::ChatServices::chats', pagUrl);
+    }
+
     const requestOptions = {
         headers: authHeader(APP_STORE.getToken()),
         method: 'GET',
